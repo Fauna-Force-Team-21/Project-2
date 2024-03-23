0e53a06d7297e6eb6167af8502fae00386db504a
dbardus <dbardus22@gmail.com>
2024-03-23 19:54:52 -0400

    Update EquationODE.m

--- EquationODE.m
+++ EquationODE.m
@@ -3,16 +3,18 @@ d = 0.00001;
 p_fluid = 1.229;
 p_particle = (2.478 * 10^(-24))/(4/3*pi*(d/2)^2);
 q = 1.6*10^-19;
-A = 4*pi*(d/2)^2;
 e0 = 8.854187817*10^-12;
 H = 3;
 Cd = 0.47;
 o = q / H^2 + H; 
 ct = 0;
+mu = 1.73*10^-5;
+%v_term = sqrt(2*g*m)
 
 gravity = (pi/6)*(p_fluid-p_particle)*g*d^3;
 buoyent = (pi/6)*(p_fluid)*g*d^3;
-drag = 0.5*p_fluid*Cd*(pi/4);
+drag = 3*pi*mu*d;
+%0.5*p_fluid*Cd*(pi/4);
 collector = (q*o)/(2*e0);
 eletric_other = (q^2*ct);
 mass = (pi/6)*p_particle*d^3;
@@ -24,16 +26,16 @@ opt = odeset('RelTol',1*exp(-8), 'AbsTol',1*exp(-8));
 [T,X] = ode45(@dynamic, tspan, x0, opt);
 
 plot(T,(X(:,1)))
+disp([T,X])
 
 function dsdt = dynamic(t,s)
     dsdt(1) = s(2);
-    dsdt(2) = (s(3) + s(4)*s(2) - s(6) + s(7)*(2*s(1) - s(8)))/s(9);
+    dsdt(2) = (s(3) + s(4) - s(5) + s(6)*(2*s(1) - s(7)))/s(8);
     dsdt(3) = 0;
     dsdt(4) = 0;
     dsdt(5) = 0;
     dsdt(6) = 0;
     dsdt(7) = 0;
     dsdt(8) = 0;
-    dsdt(9) = 0;
     dsdt = dsdt.';
 end
