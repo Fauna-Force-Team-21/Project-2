3d5aa7cd97847960ffb1a81f34a71db4d000aa6a
dbardus <dbardus22@gmail.com>
2024-03-25 10:02:20 -0400

    mn

--- Final_times.m
+++ Final_times.m
@@ -10,65 +10,48 @@ PM_10 = calculationInputs(masses(7), diameter(7));
 PM_2_5 = calculationInputs(masses(8), diameter(8));
 
 g = 9.81;
-d = diameter(7);
-p_fluid = 1.3229;
+d = diameter(8);
+p_fluid = 1.229;
 pi = 3.141592653589793;
-m = masses(7);
-p_particle = m/(4/3*pi*(d/2)^2);
+m = masses(8);
+p_particle = (m)/(4/3*pi*(d/2)^3);
 q = 1.6*10^-19;
-A = pi*(d/2)^2;
 e0 = 8.854187817*10^-12;
 H = 3;
 C_d = 0.47;
 o = q /H^2 + H; 
 ct = 0;
-u = 1.73 * 10^(-5);
-v_term =sqrt((2 * m * g) / (p_fluid * A * C_d)); % m / s
-gravity = (pi/6)*(p_fluid-p_particle)*g*d^3;
-buoyent = (pi/6)*(p_fluid)*g*d^3;
-%drag = 0.5*p_fluid*C_d*(pi/4);
-drag = (3 * pi * u * d * v_term^2);
-collector = (q*o)/(2*e0);
-eletric_other = (q^2*ct);
-mass = (pi/6)*p_particle*d^3;
+u = 1.73*10^(-5);
 
-tspan = [0, 1];
-x0 = [H, -3, gravity, buoyent, drag, collector, eletric_other, H, mass];
-opt = odeset('RelTol',1*exp(-8), 'AbsTol',1*exp(-8));
+gravity = (pi/6)*p_particle*g*d^3;
+bouyent = (pi/6)*p_fluid*g*d^3;
+drag = 0.5*p_fluid*C_d*(pi/4);
+%drag = (3 * pi * u * d);
+collector = (q*o)/(2*e0);
+Fe = collector
+eletric_other = (q^2*ct);
+%mass = (pi/6)*p_particle*d^3;
+
+tspan = [0, 1000000000000];
+x0 = [H, -3, gravity, bouyent, drag, collector, eletric_other, m];
+opt = odeset('RelTol',1e-8, 'AbsTol',1e-8);
 [T,X] = ode45(@dynamic, tspan, x0, opt);
 
-figure(1)
+figure (1)
 plot(T,X(:,1))
-ylim([0, 3]); % Adjust y-axis limits
+%ylim([0, 3]); % Adjust y-axis limits
 xlabel('Time');
 ylabel('Solution (X)');
 title('Solution Plot');
-disp([T, X])
-conts = [];
-conts(1) = 0
-for i = 2:69
-    %ct = ((pi/6)* p_particle*d^3*X(i,3) - (pi/6)*(p_fluid-p_particle)*g*d^3 - (3 * pi * u * d * ((X(i,2))^2) + (q*o)/(2*e0))) * ((2*e0)/(q^2 * (2*X(i,1)-H)));
-    ct = ((pi/6)* p_particle *d^3*(X(i,3)) - (pi/6)*(p_fluid-p_particle)*g*d^3 - (0.5*p_fluid*C_d*(pi/4) * (d)^2 * ((X(i,2) - 3)^2) + (q*o)/(2*e0))) * ((2*e0)/(q^2 * (2*(X(i,1)+H)-H)));
-    ct = abs(ct)
-    conts(i) = ct + conts(i-1); 
-end
-disp(conts)
-figure(2)
-plot(T, conts)
-xlabel("Time")
-ylabel("Concentration of Particles")
-title("Concentration of Particles by Time")
 
-disp([T,X(:,1)])
 function dsdt = dynamic(t,s)
     dsdt(1) = s(2);
-    dsdt(2) = (s(4) + s(5) - s(3) - s(6) - s(7)) / s(9);
+    dsdt(2) = (s(5)*s(2)^2 + s(4) - s(3) + s(7)*(2*s(1)-3))/(s(8) * 6.023 * 10^(23));
     dsdt(3) = 0;
     dsdt(4) = 0;
     dsdt(5) = 0;
     dsdt(6) = 0;
     dsdt(7) = 0;
     dsdt(8) = 0;
-    dsdt(9) = 0;
     dsdt = dsdt.';
 end
